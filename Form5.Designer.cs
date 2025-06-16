4874b6966b20ac9cc2ea86b1c2f27b65173feb9c
Elif Sülün <suluneliif@gmail.com>
2025-06-16 09:53:43 +0300

    Movie Trailer Project
    Menu Strip ve Web Browser (Web View) kullanarak tarayıcıdan linkteki fragmanı çeken bir arayüz oluşturdum.

---
+++ Form5.Designer.cs
@@ -0,0 +1,260 @@
+﻿namespace AracKullanimlari
+{
+    partial class Form5
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.categoriesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.horrorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oculusToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.actionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.madMaxToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.comedyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.theTrumanShowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dramaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ınterstellarToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.colorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.blueToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grayToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.orangeToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quickAccessToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutUsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.categoriesToolStripMenuItem,
+            this.colorToolStripMenuItem,
+            this.quickAccessToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(12, 3, 0, 3);
+            this.menuStrip1.Size = new System.Drawing.Size(919, 34);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // categoriesToolStripMenuItem
+            // 
+            this.categoriesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.horrorToolStripMenuItem,
+            this.actionToolStripMenuItem,
+            this.comedyToolStripMenuItem,
+            this.dramaToolStripMenuItem});
+            this.categoriesToolStripMenuItem.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.categoriesToolStripMenuItem.Name = "categoriesToolStripMenuItem";
+            this.categoriesToolStripMenuItem.Size = new System.Drawing.Size(118, 28);
+            this.categoriesToolStripMenuItem.Text = "Categories";
+            // 
+            // horrorToolStripMenuItem
+            // 
+            this.horrorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oculusToolStripMenuItem});
+            this.horrorToolStripMenuItem.Name = "horrorToolStripMenuItem";
+            this.horrorToolStripMenuItem.Size = new System.Drawing.Size(231, 28);
+            this.horrorToolStripMenuItem.Text = "Horror";
+            // 
+            // oculusToolStripMenuItem
+            // 
+            this.oculusToolStripMenuItem.Name = "oculusToolStripMenuItem";
+            this.oculusToolStripMenuItem.Size = new System.Drawing.Size(224, 28);
+            this.oculusToolStripMenuItem.Text = "Oculus";
+            this.oculusToolStripMenuItem.Click += new System.EventHandler(this.oculusToolStripMenuItem_Click);
+            // 
+            // actionToolStripMenuItem
+            // 
+            this.actionToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.madMaxToolStripMenuItem});
+            this.actionToolStripMenuItem.Name = "actionToolStripMenuItem";
+            this.actionToolStripMenuItem.Size = new System.Drawing.Size(231, 28);
+            this.actionToolStripMenuItem.Text = "Action";
+            // 
+            // madMaxToolStripMenuItem
+            // 
+            this.madMaxToolStripMenuItem.Name = "madMaxToolStripMenuItem";
+            this.madMaxToolStripMenuItem.Size = new System.Drawing.Size(178, 28);
+            this.madMaxToolStripMenuItem.Text = "Mad Max";
+            this.madMaxToolStripMenuItem.Click += new System.EventHandler(this.madMaxToolStripMenuItem_Click);
+            // 
+            // comedyToolStripMenuItem
+            // 
+            this.comedyToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.theTrumanShowToolStripMenuItem});
+            this.comedyToolStripMenuItem.Name = "comedyToolStripMenuItem";
+            this.comedyToolStripMenuItem.Size = new System.Drawing.Size(231, 28);
+            this.comedyToolStripMenuItem.Text = "Comedy";
+            // 
+            // theTrumanShowToolStripMenuItem
+            // 
+            this.theTrumanShowToolStripMenuItem.Name = "theTrumanShowToolStripMenuItem";
+            this.theTrumanShowToolStripMenuItem.Size = new System.Drawing.Size(259, 28);
+            this.theTrumanShowToolStripMenuItem.Text = "The Truman Show";
+            this.theTrumanShowToolStripMenuItem.Click += new System.EventHandler(this.theTrumanShowToolStripMenuItem_Click);
+            // 
+            // dramaToolStripMenuItem
+            // 
+            this.dramaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ınterstellarToolStripMenuItem});
+            this.dramaToolStripMenuItem.Name = "dramaToolStripMenuItem";
+            this.dramaToolStripMenuItem.Size = new System.Drawing.Size(231, 28);
+            this.dramaToolStripMenuItem.Text = "Science-Fiction";
+            // 
+            // ınterstellarToolStripMenuItem
+            // 
+            this.ınterstellarToolStripMenuItem.Name = "ınterstellarToolStripMenuItem";
+            this.ınterstellarToolStripMenuItem.Size = new System.Drawing.Size(195, 28);
+            this.ınterstellarToolStripMenuItem.Text = "Interstellar";
+            this.ınterstellarToolStripMenuItem.Click += new System.EventHandler(this.ınterstellarToolStripMenuItem_Click);
+            // 
+            // colorToolStripMenuItem
+            // 
+            this.colorToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.blueToolStripMenuItem,
+            this.grayToolStripMenuItem,
+            this.orangeToolStripMenuItem});
+            this.colorToolStripMenuItem.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.colorToolStripMenuItem.Name = "colorToolStripMenuItem";
+            this.colorToolStripMenuItem.Size = new System.Drawing.Size(73, 28);
+            this.colorToolStripMenuItem.Text = "Color";
+            // 
+            // blueToolStripMenuItem
+            // 
+            this.blueToolStripMenuItem.Name = "blueToolStripMenuItem";
+            this.blueToolStripMenuItem.Size = new System.Drawing.Size(159, 28);
+            this.blueToolStripMenuItem.Text = "Blue";
+            this.blueToolStripMenuItem.Click += new System.EventHandler(this.blueToolStripMenuItem_Click);
+            // 
+            // grayToolStripMenuItem
+            // 
+            this.grayToolStripMenuItem.Name = "grayToolStripMenuItem";
+            this.grayToolStripMenuItem.Size = new System.Drawing.Size(159, 28);
+            this.grayToolStripMenuItem.Text = "Gray";
+            this.grayToolStripMenuItem.Click += new System.EventHandler(this.grayToolStripMenuItem_Click);
+            // 
+            // orangeToolStripMenuItem
+            // 
+            this.orangeToolStripMenuItem.Name = "orangeToolStripMenuItem";
+            this.orangeToolStripMenuItem.Size = new System.Drawing.Size(159, 28);
+            this.orangeToolStripMenuItem.Text = "Orange";
+            this.orangeToolStripMenuItem.Click += new System.EventHandler(this.orangeToolStripMenuItem_Click);
+            // 
+            // quickAccessToolStripMenuItem
+            // 
+            this.quickAccessToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutUsToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.quickAccessToolStripMenuItem.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.quickAccessToolStripMenuItem.Name = "quickAccessToolStripMenuItem";
+            this.quickAccessToolStripMenuItem.Size = new System.Drawing.Size(141, 28);
+            this.quickAccessToolStripMenuItem.Text = "Quick Access";
+            // 
+            // aboutUsToolStripMenuItem
+            // 
+            this.aboutUsToolStripMenuItem.Name = "aboutUsToolStripMenuItem";
+            this.aboutUsToolStripMenuItem.Size = new System.Drawing.Size(177, 28);
+            this.aboutUsToolStripMenuItem.Text = "About Us";
+            this.aboutUsToolStripMenuItem.Click += new System.EventHandler(this.aboutUsToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(177, 28);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 16F);
+            this.label1.Location = new System.Drawing.Point(49, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 31);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(55, 111);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(809, 417);
+            this.webView21.TabIndex = 3;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Form5
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(919, 561);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Georgia", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form5";
+            this.Text = "Movie Trailer Project";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem categoriesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem colorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem horrorToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oculusToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem actionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem madMaxToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem comedyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem theTrumanShowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem dramaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ınterstellarToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem blueToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem grayToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem orangeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quickAccessToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutUsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
---
+++ Form5.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace AracKullanimlari
+{
+    public partial class Form5 : Form
+    {
+        public Form5()
+        {
+            InitializeComponent();
+        }
+
+        private void blueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.CadetBlue;
+        }
+
+        private void grayToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Gray;
+        }
+
+        private void orangeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Orange;
+        }
+
+        private void aboutUsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("This is a sample application demonstrating menu usage in C# Windows Forms.", "About Us", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void oculusToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://youtu.be/dYJrxezWLUk?si=HaZX3iE2eiU4leH9");
+            label1.Text = oculusToolStripMenuItem.Text;
+        }
+
+        private void madMaxToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://youtu.be/hEJnMQG9ev8?si=2b1j4a0g3k6c7d5B");
+            label1.Text = madMaxToolStripMenuItem.Text;
+        }
+
+        private void theTrumanShowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://youtu.be/dlnmQbPGuls?si=qtxtUhS98WPTrtK2");
+            label1.Text = theTrumanShowToolStripMenuItem.Text;
+        }
+
+        private void ınterstellarToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri("https://youtu.be/zSWdZVtXT7E?si=puW8Y5cSTrYy40xB");
+            label1.Text = ınterstellarToolStripMenuItem.Text;
+        }
+    }
+}
